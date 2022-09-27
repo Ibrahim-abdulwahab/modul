7c213eaea320e3d40ad553bd18d880081f882b1b
[Ibrahim Abdulwahab] <[ibrahim.abdulwahab1997@gmail.com]>
2022-09-27 23:13:59 +0300

    finished the script part

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-window.addEventListener('load', function(){
+const btn= document.getElementById("btn");
+const main = document.getElementById("main");
+const project=document.getElementById("project");
+const content=document.getElementById("content");
+const close=document.getElementById("close");
 
 
+const blue = ()=>{
+    main.className="blue";
+    project.className+=" blue";
+    content.className="modal-content"      
+}
+btn.addEventListener("click",blue)
 
-    
-})
+const undo = ()=>{
+main.className="";
+project.className="modul-project"
+content.className="modal-content none"  
+}
+close.addEventListener("click",undo)
