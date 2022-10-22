b8506488d747decb302d0090d4109ed5925d1b72
[Ibrahim Abdulwahab] <[ibrahim.abdulwahab1997@gmail.com]>
2022-10-22 19:47:57 +0300

    more efficient javscript

--- script.js
+++ script.js
@@ -6,15 +6,18 @@ const close=document.getElementById("close");
 
 
 const blue = ()=>{
-    main.className="blue";
-    project.className+=" blue";
-    content.className="modal-content"      
+    // main.className="blue";
+    // project.className+=" blue";
+    // content.className="modal-content"
+    main.classList.toggle("blue");      
+    project.classList.toggle("blue");      
+    content.classList.toggle("none");      
 }
 btn.addEventListener("click",blue)
 
-const undo = ()=>{
-main.className="";
-project.className="modul-project"
-content.className="modal-content none"  
-}
-close.addEventListener("click",undo)
+// const undo = ()=>{
+// main.className="";
+// project.className="modul-project"
+// content.className="modal-content none"  
+// }
+close.addEventListener("click",blue)
